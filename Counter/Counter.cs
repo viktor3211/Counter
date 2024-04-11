1e960296a1d5863962a635bf63be92db490ee619
Viktor Reuterhäll <viktor.reuterhall@skola.sala.se>
2024-04-11 14:11:21 +0200

    Add project files.

---
+++ Counter/Counter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Counter
+{
+    
+        public class Counter
+        {
+            
+            int value = 0;
+            int counter = 0;
+
+            // ökar räknaren med ett
+            public void Increment()
+            {
+               counter++;
+               value = counter;
+            }
+
+            // återställer värdet till noll
+            public void Reset()
+            {
+                value = 0;
+            }
+
+            // ger värdet på räknaren
+            public int GetValue()
+            {
+                
+                return value;
+            }
+        }
+
+
+    
+}
---
+++ Counter/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Counter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Counter counter = new Counter();
+            counter.Increment();
+            counter.Increment();
+            counter.Increment();
+            // ska skriva ut 3
+            Console.WriteLine("counter: " + counter.GetValue());
+            counter.Reset();
+            // ska skriva ut 0
+            Console.WriteLine("counter: " + counter.GetValue());
+        }
+
+
+    }
+}
